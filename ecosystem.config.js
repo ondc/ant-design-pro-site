1dc99985ec4343af1d133874f46059b6b3a4eb65
leo <fugang.bj@qq.com>
2018-04-24 18:49:21 +0800

    package lock

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,64 +1,32 @@
 module.exports = {
-  /**
-   * Application configuration section
-   * http://pm2.keymetrics.io/docs/usage/application-declaration/
-   */
-  apps : [
-
-    // First application
+  apps: [
     {
-      name      : 'zuk-doc',
+      name: 'zuk-doc',
       script: 'npm',
       args: 'run start',
-      "cwd": "./",
-      "watch":[
-        "src",
-        "public"
-      ],//需要监控的目录
-      "error_file":"./logs/app-err.log",
-      "out_file":"./logs/app-out.log",  
-      "log_date_format":"YYYY-MM-DD HH:mm Z", 
+      cwd: './',
+      watch: [
+        'src',
+        'public'
+      ],
+      error_file: './logs/app-err.log',
+      out_file: './logs/app-out.log',  
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
       env: {
         COMMON_VARIABLE: 'true'
       },
-      env_production : {
+      env_production: {
         NODE_ENV: 'production',
-        PORT:8001
+        PORT: '8001'
       },
-      env_testing : {
+      env_testing: {
         NODE_ENV: 'testing',
-        PORT:8001
+        PORT: '8001'
       },
-      env_development : {
+      env_development: {
         NODE_ENV: 'development',
-        PORT:8001
+        PORT: '8001'
       }
     }
-  ],
-
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-   */
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    },
-    dev : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/development',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-      env  : {
-        NODE_ENV: 'dev'
-      }
-    }
-  }
+  ]
 };
